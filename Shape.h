883ea1057127a0dd6f5001ad603128abfc51b8d4
Ryan-B-Runo <43120875+Ryan-B-Runo@users.noreply.github.com>
2024-10-08 15:08:19 -0500

    create Shape.h

---
+++ Shape.h
@@ -0,0 +1,10 @@
+#ifndef CSC122SHAPESLAB_SHAPE_H
+#define CSC122SHAPESLAB_SHAPE_H
+
+class Shape {
+public:
+    virtual double getArea() = 0;
+    virtual double getPerimeter() = 0;
+};
+
+#endif //CSC122SHAPESLAB_SHAPE_H
